2333eb7642d80d8850129ed815d594688481e821
WALUNJ1710 <walunjpratik2001@gmail.com>
2026-06-01 18:50:08 +0530

    Connection checking and trying again to connect

--- portfolio_backend/portfolio_project/settings.py
+++ portfolio_backend/portfolio_project/settings.py
@@ -52,6 +52,9 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
